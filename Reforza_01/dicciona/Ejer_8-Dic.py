4b7af0eb40dd3fa7daf8f23c0c4851aa21c3e1cd
unknown <soporte.tisi365@gmail.comgit config --global user.name soporte365>
2024-09-09 18:29:20 -0500

    Actualziación de Ejer_8-dic

--- Reforza_01/dicciona/Ejer_8-Dic.py
+++ Reforza_01/dicciona/Ejer_8-Dic.py
@@ -3,3 +3,14 @@
 - Mostrar en consola los valores de su variable final (ya sea diccionario o lista).
 
 """
+
+# Dicionario inicial
+dicio = {"Nombre":"Juan","Apellido":"Perez", "Carera":"Contador","Edad":34}
+
+# imprime el diccionario inicial
+print(dicio)
+
+#Se anexa nuevo valor
+dicio["Salario"]="Ingeniría en computación"
+print("Se ha añadido un nuevo valor")
+print(dicio)
